b2d5e84f051527ac0ffdd1db020009ea961e0653
suyanto98 <suyantolearn09@gmail.com>
2024-07-23 20:45:42 +0700

    feat: slicing sign up and sign in

---
+++ resources/js/Components/Label.jsx
@@ -0,0 +1,7 @@
+export default function Label({ forInput ,value, className, children }) {
+    return (
+        <label htmlFor={forInput} className={`text-base block mb-2` + className}>
+            {value ? value : children}
+        </label>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/prototype/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -22,6 +15,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+
+    route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
 });
 
 require __DIR__.'/auth.php';
